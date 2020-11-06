603cf258067a871b6365114079442af1d3c92cf8
itshuien <huien_wong@hotmail.com>
2020-11-06 20:38:40 +0800

    Change database engine from sqlite3 to postgresql_psycopg2

--- lyrics_notation/settings.py
+++ lyrics_notation/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'lyrics_notation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_DATABASE'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
