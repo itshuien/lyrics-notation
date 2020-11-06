e93371f793cf0fae1359a4ec25e68e89fe13b834
itshuien <huien_wong@hotmail.com>
2020-11-06 20:26:29 +0800

    Initial commit

---
+++ lyrics_notation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for lyrics_notation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lyrics_notation.settings')
+
+application = get_asgi_application()
