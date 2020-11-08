77b41811aa045c28b48932eddf95088020fbac01
itshuien <huien_wong@hotmail.com>
2020-11-08 14:30:13 +0800

    Add lyrics app to project settings and urls

--- lyrics_notation/settings.py
+++ lyrics_notation/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lyrics.apps.LyricsConfig',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kuala_Lumpur'
 
 USE_I18N = True
 
