542a96c761ac2b7df75d2649a9ffdcf41ccfdc93
itshuien <huien_wong@hotmail.com>
2020-11-09 15:58:54 +0800

    Tokenize and display lyrics

--- lyrics/static/lyrics/javascript/lyricNotations.js
+++ lyrics/static/lyrics/javascript/lyricNotations.js
@@ -32,6 +32,28 @@ const loadLyricNotations = lyricNotations => {
   }
 }
 
+const loadTokenizedLyrics = () => {
+  const lyricContainer = document.getElementById('lyricContainer');
+  let lyricLineHeight = 0;
+
+  for (let lyricLine of lyricContainer.children) {
+    lyricLine.setAttribute('y', lyricLineHeight);
+    lyricLineHeight += 30;
+
+    let x = 0;
+    let lineOffset = 0;
+
+    for (let word of Array.from(lyricLine.children).filter(child => child.dataset.wordId)) {
+      word.setAttribute('x', x);
+      word.setAttribute('data-line-offset', lineOffset)
+      x += word.getBBox().width;
+      lineOffset += word.textContent.length;
+    }
+  }
+  lyricContainer.style.height = lyricLineHeight;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   loadLyricNotations(lyricNotations); // parameter is declared in template
+  loadTokenizedLyrics();
 })
--- lyrics/views.py
+++ lyrics/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Lyric
+from .utils import words_tokenizer
 
 import json
 
@@ -10,7 +11,8 @@ def show(request, pk):
 
     context = {
         'lyric': lyric,
-        'lyric_notations': json.dumps([record for record in lyric_notations.values()])
+        'lyric_notations': json.dumps([record for record in lyric_notations.values()]),
+        'tokenized_lyric_lines': [words_tokenizer(line) for line in lyric.lines]
     }
 
     return render(request, 'lyrics/show.html', context)
