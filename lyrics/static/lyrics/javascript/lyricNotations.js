2e6e1d0f704c9035be2846d5e770e94caa99af59
itshuien <huien_wong@hotmail.com>
2020-11-10 15:36:42 +0800

    Load lyric notations as rect elements

--- lyrics/static/lyrics/javascript/lyricNotations.js
+++ lyrics/static/lyrics/javascript/lyricNotations.js
@@ -41,19 +41,74 @@ const loadTokenizedLyrics = () => {
     lyricLineHeight += 30;
 
     let x = 0;
-    let lineOffset = 0;
+    let charOffset = 0;
 
     for (let word of Array.from(lyricLine.children).filter(child => child.dataset.wordId)) {
       word.setAttribute('x', x);
-      word.setAttribute('data-line-offset', lineOffset)
+      word.setAttribute('data-start-offset', charOffset)
+      word.setAttribute('data-end-offset', charOffset + word.textContent.length)
       x += word.getBBox().width;
-      lineOffset += word.textContent.length;
+      charOffset += word.textContent.length;
     }
   }
   lyricContainer.style.height = lyricLineHeight;
+  lyricContainer.style.display = '';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   loadLyricNotations(lyricNotations); // parameter is declared in template
   loadTokenizedLyrics();
+
+  document.getElementById('lyric').style.display = 'none';
+
+  const lyricModeSwitch = document.getElementById('lyricModeSwitch');
+  lyricModeSwitch.addEventListener('click', () => {
+    const tokenizedLyricContainer = document.getElementById('lyricContainer');
+    const defaultLyricContainer = document.getElementById('lyric');
+
+    if (tokenizedLyricContainer.style.display == 'none') {
+      tokenizedLyricContainer.style.display = '';
+      defaultLyricContainer.style.display = 'none';
+    } else {
+      tokenizedLyricContainer.style.display = 'none';
+      defaultLyricContainer.style.display = '';
+    }
+  })
+
+  loadLyricNotations2(lyricNotations);
 })
+
+const drawLyricNotation = (width, x) => {
+  const rectHtml = `<rect width="${width}" height="100%" x="${x}" class="rectNotation"></rect>`;
+  return rectHtml;
+}
+
+const loadLyricNotations2 = lyricNotations => {
+  for (let notation of lyricNotations) {
+    const { start_line: startLine, start_offset: startOffset, end_line: endLine, end_offset: endOffset } = notation;
+  
+    for (let lineNumber = startLine; lineNumber <= endLine; lineNumber++) {
+      const lineElement = document.querySelector(`[data-line-id="${lineNumber}"]`);
+  
+      const lineStartOffset = lineNumber == startLine ? startOffset : 0;
+      const lineEndOffset = lineNumber == endLine ? endOffset : parseInt(lineElement.lastElementChild.dataset.startOffset) + lineElement.lastElementChild.textContent.toString().length;
+
+      let startWord, endWord;
+
+      for (let wordElement of lineElement.children) {
+        if (startWord && endWord) break;
+
+        if (lineStartOffset >= wordElement.dataset.startOffset && lineStartOffset < wordElement.dataset.endOffset) {
+          startWord = wordElement;
+        } else if (lineEndOffset > wordElement.dataset.startOffset && lineEndOffset <= wordElement.dataset.endOffset) {
+          endWord = wordElement;
+        }
+      }
+
+      const startPosition = startWord.getAttribute('x');
+      const endPosition = endWord.getEndPositionOfChar(lineEndOffset - parseInt(endWord.dataset.startOffset) - 1).x;
+      const rect = drawLyricNotation((endPosition - startPosition), startPosition)
+      lineElement.insertAdjacentHTML('afterbegin', rect);
+    }
+  }
+}
