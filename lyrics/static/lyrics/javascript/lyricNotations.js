98b20afb77f255d54965effc68eff4c3b231a3c3
itshuien <huien_wong@hotmail.com>
2020-11-11 14:21:51 +0800

    Check if selected text overlaps with existing lyric notations

--- lyrics/static/lyrics/javascript/lyricNotations.js
+++ lyrics/static/lyrics/javascript/lyricNotations.js
@@ -35,21 +35,23 @@ const loadLyricNotations = lyricNotations => {
 const loadTokenizedLyrics = () => {
   const lyricContainer = document.getElementById('lyricContainer');
   let lyricLineHeight = 0;
+  let lineCharOffset = 0;
 
   for (let lyricLine of lyricContainer.children) {
     lyricLine.setAttribute('y', lyricLineHeight);
+    lyricLine.setAttribute('data-char-offset', lineCharOffset)
     lyricLineHeight += 30;
 
     let x = 0;
-    let charOffset = 0;
+    let wordCharOffset = 0;
 
     for (let word of Array.from(lyricLine.children).filter(child => child.dataset.wordId)) {
       word.setAttribute('x', x);
-      word.setAttribute('data-start-offset', charOffset)
-      word.setAttribute('data-end-offset', charOffset + word.textContent.length)
+      word.setAttribute('data-char-offset', wordCharOffset)
       x += word.getBBox().width + 2;
-      charOffset += word.textContent.length;
+      wordCharOffset += word.textContent.length;
     }
+    lineCharOffset += wordCharOffset;
   }
   lyricContainer.style.height = lyricLineHeight;
   lyricContainer.style.display = '';
@@ -80,8 +82,8 @@ document.addEventListener('DOMContentLoaded', () => {
   loadLyricNotations2(lyricNotations);
 })
 
-const drawLyricNotation = (width, x) => {
-  const rectHtml = `<rect width="${width}" height="100%" x="${x}" class="rectNotation"></rect>`;
+const drawLyricNotation = (line, startWord, endWord, width, x) => {
+  const rectHtml = `<rect data-line-id="${line}" data-first-word="${startWord}" data-last-word="${endWord}"  width="${width}" height="100%" x="${x}" class="rectNotation"></rect>`;
   return rectHtml;
 }
 
@@ -93,26 +95,29 @@ const loadLyricNotations2 = lyricNotations => {
       const lineElement = document.querySelector(`[data-line-id="${lineNumber}"]`);
   
       const lineStartOffset = lineNumber == startLine ? startOffset : 0;
-      const lineEndOffset = lineNumber == endLine ? endOffset : parseInt(lineElement.lastElementChild.dataset.startOffset) + lineElement.lastElementChild.textContent.toString().length;
+      const lineEndOffset = lineNumber == endLine ? endOffset : parseInt(lineElement.lastElementChild.dataset.charOffset) + lineElement.lastElementChild.textContent.toString().length;
 
       let startWord, endWord;
 
       for (let wordElement of lineElement.children) {
         if (startWord && endWord) break;
 
-        if (!startWord && lineStartOffset >= wordElement.dataset.startOffset && lineStartOffset < wordElement.dataset.endOffset) {
+        const wordCharOffset = parseInt(wordElement.dataset.charOffset);
+        const wordLength = wordElement.textContent.length + wordCharOffset;
+
+        if (!startWord && lineStartOffset >= wordCharOffset && lineStartOffset < wordLength) {
           startWord = wordElement;
         }
-        if (!endWord && lineEndOffset > wordElement.dataset.startOffset && lineEndOffset <= wordElement.dataset.endOffset) {
+        if (!endWord && lineEndOffset > wordCharOffset && lineEndOffset <= wordLength) {
           endWord = wordElement;
         }
       }
       
       if (!startWord || !endWord) continue;
 
-      const startPosition = startWord.getStartPositionOfChar(lineStartOffset - parseInt(startWord.dataset.startOffset)).x;
-      const endPosition = endWord.getEndPositionOfChar(lineEndOffset - parseInt(endWord.dataset.startOffset) - 1).x;
-      const rect = drawLyricNotation((endPosition - startPosition), startPosition)
+      const startPosition = startWord.getStartPositionOfChar(lineStartOffset - parseInt(startWord.dataset.charOffset)).x;
+      const endPosition = endWord.getEndPositionOfChar(lineEndOffset - parseInt(endWord.dataset.charOffset) - 1).x;
+      const rect = drawLyricNotation(lineNumber, startWord.dataset.wordId, endWord.dataset.wordId, (endPosition - startPosition), startPosition)
       lineElement.insertAdjacentHTML('afterbegin', rect);
     }
   }
@@ -131,13 +136,57 @@ const magneticSelectionOnMouseUp = () => {
   
   lyricContainer.addEventListener('mouseup', () => {
     const selection = window.getSelection();
-    const { baseNode, extentNode } = selection;
+    const { startNode, endNode } = getSelectionDetails();
 
     const range = new Range();
-    range.setStart(baseNode.parentNode.firstChild, 0);
-    range.setEnd(extentNode.parentNode.firstChild, extentNode.textContent.length);
+    range.setStart(startNode.firstChild, 0);
+    range.setEnd(endNode.firstChild, endNode.textContent.length);
 
     selection.removeAllRanges();
     selection.addRange(range);
+
+    console.log(`Overlapping: ${isOverlappingWithExistingNotations()}`)
   })
+}
+
+const getSelectionNodes = () => {
+  let { baseNode, extentNode } = window.getSelection();
+
+  baseNode = baseNode.parentNode.dataset.lineId ? baseNode : baseNode.parentNode;
+  extentNode = extentNode.parentNode.dataset.lineId ? extentNode : extentNode.parentNode;
+
+  return { baseNode, extentNode }
+}
+
+const getSelectionDetails = () => {
+  let { baseNode, extentNode } = getSelectionNodes();
+
+  let startCharOffset = parseInt(baseNode.parentNode?.dataset?.charOffset) + parseInt(baseNode.dataset?.charOffset);
+  let endCharOffset = parseInt(extentNode.parentNode?.dataset?.charOffset) + parseInt(extentNode.textContent?.length - extentNode.dataset?.charOffset);
+
+  if (startCharOffset > endCharOffset) {
+    [startCharOffset, endCharOffset] = [endCharOffset, startCharOffset];
+    [baseNode, extentNode] = [extentNode, baseNode];
+  }
+
+  return { startNode: baseNode, endNode: extentNode, startCharOffset, endCharOffset };
+}
+
+const isOverlappingWithExistingNotations = () => {
+  const selection = getSelectionDetails();
+
+  const start1 = selection.startCharOffset;
+  const end1 = selection.endCharOffset;
+
+  for (let notation of lyricNotations) {
+    const start2 = parseInt(document.querySelector(`[data-line-id="${notation.start_line}"]`).dataset.charOffset) + notation.start_offset;
+    const end2 = parseInt(document.querySelector(`[data-line-id="${notation.end_line}"]`).dataset.charOffset) + notation.end_offset;
+
+    if ((start1 <= start2 && end1 >= end2 ||
+    start1 >= start2 && end1 <= end2 ||
+    start1 >= start2 && start1 < end2 ||
+    end1 <= end2 && end1 > start2)) return true;
+  };
+
+  return false;
 }
