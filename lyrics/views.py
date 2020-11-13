97457d873908cc8a416d57a6893b182c42ca8e76
itshuien <huien_wong@hotmail.com>
2020-11-13 21:03:43 +0800

    Modify url for removing lyric notation

--- lyrics/views.py
+++ lyrics/views.py
@@ -34,8 +34,8 @@ def add_lyric_notation(request, lyric_id):
 
     return HttpResponseRedirect(reverse('lyrics:show', args=(lyric_id,)))
 
-def remove_lyric_notation(request, lyric_id, lyric_notation_id):
+def remove_lyric_notation(request, lyric_notation_id):
     lyric_notation = LyricNotation.objects.get(pk=lyric_notation_id)
     lyric_notation.delete()
 
-    return HttpResponseRedirect(reverse('lyrics:show', args=(lyric_id,)))
+    return HttpResponseRedirect(reverse('lyrics:show', args=(lyric_notation.lyric_id,)))
