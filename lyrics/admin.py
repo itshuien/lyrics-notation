2a7403f14e4d3104cdba693abe07d0e2d90c95ad
itshuien <huien_wong@hotmail.com>
2020-11-17 14:41:59 +0800

    Register LyricNotation and PhoneticNotation models with admin site

--- lyrics/admin.py
+++ lyrics/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Lyric
+from .models import Lyric, LyricNotation, PhoneticNotation
 from .forms import LyricForm
 
 class LyricAdmin(admin.ModelAdmin):
@@ -11,3 +11,5 @@ class LyricAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 admin.site.register(Lyric, LyricAdmin)
+admin.site.register(LyricNotation)
+admin.site.register(PhoneticNotation)
