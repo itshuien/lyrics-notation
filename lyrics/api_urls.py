74a9a11d1d11a0ab55e173c39d2c6ea73020d4f4
itshuien <huien_wong@hotmail.com>
2021-03-09 11:03:50 +0800

    Implement GET endpoints for lyric notations and phonetic notations

--- lyrics/api_urls.py
+++ lyrics/api_urls.py
@@ -6,5 +6,7 @@ app_name = 'lyrics'
 
 urlpatterns = [
     path('lyrics/', api_views.lyrics),
-    path('lyrics/<int:pk>', api_views.lyric),
+    path('lyrics/<int:pk>/', api_views.lyric),
+    path('lyrics/<int:lyric_id>/lyric_notations/', api_views.lyric_notations),
+    path('lyrics/<int:lyric_id>/phonetic_notations/', api_views.phonetic_notations),
 ]
--- lyrics/api_views.py
+++ lyrics/api_views.py
@@ -3,8 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import Lyric
-from .serializers import LyricSerializer
+from .serializers import LyricSerializer, LyricNotationSerializer, PhoneticNotationnSerializer
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -27,3 +29,37 @@ def lyric(request, pk):
 
     serializer = LyricSerializer(lyric)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def lyric_notations(request, lyric_id):
+    try:
+        lyric = Lyric.objects.get(pk=lyric_id)
+
+        if not request.user.is_superuser and request.user != lyric.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        lyric_notations = lyric.lyricnotation_set.all()
+        serializer = LyricNotationSerializer(lyric_notations, many=True)
+        
+        return Response(serializer.data)
+
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def phonetic_notations(request, lyric_id):
+    try:
+        lyric = Lyric.objects.get(pk=lyric_id)
+
+        if not request.user.is_superuser and request.user != lyric.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        phonetic_notations = lyric.phoneticnotation_set.all()
+        serializer = PhoneticNotationnSerializer(phonetic_notations, many=True)
+        
+        return Response(serializer.data)
+
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
